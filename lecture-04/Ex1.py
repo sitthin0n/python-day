41b987ac506f375e2991e78eb4da9288eb99a2de
sitthin0n <s6906022610801@email.kmutnb.ac.th>
2026-07-23 10:06:08 +0700

    7/23/26

---
+++ lecture-04/Ex1.py
@@ -0,0 +1,7 @@
+print("--------")
+print("KPH\tMPH")
+print("--------")
+for Kph in range(60,140, 10):
+    Mph = Kph*0.6214
+    print(Kph, '\t', Mph)
+    
---
+++ lecture-04/Ex2.py
@@ -0,0 +1,7 @@
+Row = int(input("How many rows: "))
+colum = int(input("How many columns: "))
+for i in range(Row):
+    for j in range(colum):
+        print("*", end="")
+    print()
+    
